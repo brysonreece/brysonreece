2ceeee537a4d4572fc16ab47b23f37c0b011a0fc
Bryson Reece <hey@bryson.cc>
2024-01-26 23:28:01 -0600

    Add middleware for local-only requests

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
+    'local',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
