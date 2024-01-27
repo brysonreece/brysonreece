075eb7af9db0d24d01604b3d507992aa8e2eb265
Bryson Reece <hey@bryson.cc>
2024-01-26 23:58:50 -0600

    Store last login details for user accounts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('current_team_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('last_login_ip')->nullable();
+            $table->timestamp('last_login_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
--- resources/types.ts
+++ resources/types.ts
@@ -19,6 +19,8 @@ export interface User {
   profile_photo_url: string;
   two_factor_enabled: boolean;
   email_verified_at: Nullable<DateTime>;
+  last_login_at: Nullable<DateTime>;
+  last_login_ip: Nullable<string>;
   created_at: DateTime;
   updated_at: DateTime;
 }
