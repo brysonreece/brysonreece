dd76da58abc7b0625ed723e8b62ad0aafa8ff6ac
Bryson Reece <hey@bryson.cc>
2025-02-02 14:36:43 -0600

    Update app title scheme

--- resources/app.tsx
+++ resources/app.tsx
@@ -1,16 +1,14 @@
 import './bootstrap';
 import './styles/app.scss';
 
-import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText
-    || import.meta.env.VITE_APP_NAME
-    || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => (title ? `${title} // ${appName}` : appName),
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         return createRoot(el).render(<App {...props} />);
