3f4a6e46704448b27532e5aaf0dd11bbd4d706d8
Bryson Reece <hey@bryson.cc>
2025-02-02 14:40:34 -0600

    Wrap app in tooltip provider

--- resources/app.tsx
+++ resources/app.tsx
@@ -5,15 +5,27 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
+import { TooltipProvider } from './components/ui/tooltip';
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} // ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./pages/${name}.tsx`,
+            import.meta.glob('./pages/**/*.tsx'),
+        ),
     setup({ el, App, props }) {
-        return createRoot(el).render(<App {...props} />);
+        const root = createRoot(el);
+
+        root.render(
+            <TooltipProvider>
+                <App {...props} />
+            </TooltipProvider>,
+        );
     },
     progress: {
-      color: '#4B5563',
+        color: '#4b5563',
     },
 });
