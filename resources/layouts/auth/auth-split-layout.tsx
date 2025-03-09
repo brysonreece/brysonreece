b9699f1d2bc0675d021ef0853f835f2a0ef32128
Bryson Reece <hey@bryson.cc>
2025-03-09 16:43:22 -0500

    Update application logo

--- resources/layouts/auth/auth-split-layout.tsx
+++ resources/layouts/auth/auth-split-layout.tsx
@@ -1,5 +1,6 @@
+import { appName } from '@/app';
 import AppLogoIcon from '@/components/app/app-logo-icon';
-import { Link, usePage } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
 interface AuthLayoutProps {
@@ -8,15 +9,13 @@ interface AuthLayoutProps {
 }
 
 export default function AuthSplitLayout({ children, title, description }: PropsWithChildren<AuthLayoutProps>) {
-    const { name, quote } = usePage().props;
-
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
                 <Link href={route('dashboard')} className="relative z-20 flex items-center text-lg font-medium">
-                    <AppLogoIcon className="mr-2 size-8 fill-current text-white" />
-                    {name}
+                    <AppLogoIcon className="mr-2 size-8 fill-transparent stroke-white" />
+                    {appName}
                 </Link>
                 <div className="relative z-20 mt-auto">
                     <blockquote className="space-y-2">
@@ -28,7 +27,7 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
             <div className="w-full lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <Link href={route('dashboard')} className="relative z-20 flex items-center justify-center lg:hidden">
-                        <AppLogoIcon className="h-10 fill-current text-black sm:h-12" />
+                        <AppLogoIcon className="h-10 fill-transparent stroke-black sm:h-12" />
                     </Link>
                     <div className="flex flex-col items-start gap-2 text-left sm:items-center sm:text-center">
                         <h1 className="text-xl font-medium">{title}</h1>
