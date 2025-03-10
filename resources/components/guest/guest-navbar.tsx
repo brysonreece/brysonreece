0cd5d2cb7398adf1de7686181b60ba40ba378685
Bryson Reece <hey@bryson.cc>
2025-03-10 11:34:04 -0500

    Rename guest navbar component

--- resources/components/guest/guest-navbar.tsx
+++ resources/components/guest/guest-navbar.tsx
@@ -1,8 +1,7 @@
-import { PropsWithChildren } from 'react';
-import { Disclosure } from '@headlessui/react'
 import clsx from 'clsx'
 import { Link } from '@inertiajs/react';
 import { MenuIcon, XIcon } from 'lucide-react';
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 
 const navigation = [
     { name: 'About', route: 'about' },
@@ -12,11 +11,11 @@ const navigation = [
     { name: 'Uses', route: 'uses' },
 ]
 
-interface Props {
-  showLogo?: boolean;
+interface GuestNavbarProps {
+    showLogo?: boolean
 }
 
-export function Navbar({ showLogo = true }: PropsWithChildren<Props>) {
+export function GuestNavbar({ showLogo = true }: GuestNavbarProps) {
     return (
         <Disclosure as="nav" className="w-full max-w-5xl mx-auto p-2 sm:p-6 lg:p-8">
             {({ open }) => (
@@ -50,14 +49,14 @@ export function Navbar({ showLogo = true }: PropsWithChildren<Props>) {
                                 </Link>
                             )}
 
-                            <Disclosure.Button className="ml-auto relative sm:hidden inline-flex items-center justify-center p-4 rounded-md text-stone-400 hover:text-stone-500 dark:text-stone-500 dark:hover:text-stone-400 -outline-offset-4">
+                            <DisclosureButton className="ml-auto relative sm:hidden inline-flex items-center justify-center p-4 rounded-md text-stone-400 hover:text-stone-500 dark:text-stone-500 dark:hover:text-stone-400 -outline-offset-4">
                                 <span className="sr-only">Open main menu</span>
                                 {open ? (
                                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                                 ) : (
                                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                                 )}
-                            </Disclosure.Button>
+                            </DisclosureButton>
 
                             <div className="hidden sm:flex sm:space-x-4 sm:p-2">
                                 {navigation.map((item) => (
@@ -76,44 +75,44 @@ export function Navbar({ showLogo = true }: PropsWithChildren<Props>) {
                             </div>
                         </div>
 
-                        <Disclosure.Panel className="block sm:hidden p-2">
-                          <div className="flex flex-col items-end space-y-2 text-right">
-                              {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as={Link}
-                                    href={route(item.route)}
-                                    className={clsx(
-                                        // light - plain, hover, active
-                                          // text: 500, 700, 900
-                                          // decoration: n/a, 400, 500
-                                        // dark - plain, hover, active
-                                          // text: 500, 400, 300
-                                          // decoration: n/a, 700, 500
-                                        route().current(item.route) ? [
-                                          'text-stone-900 decoration-stone-500',
-                                          'dark:text-stone-300 dark:decoration-stone-500',
-                                          'underline underline-offset-8 decoration-2',
-                                        ] : [
-                                          'text-stone-500',
-                                          'hover:text-stone-700 hover:decoration-stone-400',
-                                          'dark:hover:text-stone-400 dark:hover:decoration-stone-700',
-                                          'hover:underline hover:underline-offset-8 hover:decoration-2',
-                                        ],
-                                        [
-                                          'active:text-stone-900 active:decoration-stone-500',
-                                          'dark:active:text-stone-300 dark:active:decoration-stone-500',
-                                          'dark:active:underline dark:active:underline-offset-8 dark:active:decoration-2',
-                                          'rounded-lg px-3 py-2 text-sm font-medium block w-1/3',
-                                        ],
-                                    )}
-                                    aria-current={route().current(item.route) ? 'page' : undefined}
-                                >
-                                    {item.name}
-                                </Disclosure.Button>
-                              ))}
-                          </div>
-                        </Disclosure.Panel>
+                        <DisclosurePanel className="block sm:hidden p-2">
+                            <div className="flex flex-col items-end space-y-2 text-right">
+                                {navigation.map((item) => (
+                                    <DisclosureButton
+                                        key={item.name}
+                                        as={Link}
+                                        href={route(item.route)}
+                                        className={clsx(
+                                            // light - plain, hover, active
+                                            // text: 500, 700, 900
+                                            // decoration: n/a, 400, 500
+                                            // dark - plain, hover, active
+                                            // text: 500, 400, 300
+                                            // decoration: n/a, 700, 500
+                                            route().current(item.route) ? [
+                                                'text-stone-900 decoration-stone-500',
+                                                'dark:text-stone-300 dark:decoration-stone-500',
+                                                'underline underline-offset-8 decoration-2',
+                                            ] : [
+                                                'text-stone-500',
+                                                'hover:text-stone-700 hover:decoration-stone-400',
+                                                'dark:hover:text-stone-400 dark:hover:decoration-stone-700',
+                                                'hover:underline hover:underline-offset-8 hover:decoration-2',
+                                            ],
+                                            [
+                                                'active:text-stone-900 active:decoration-stone-500',
+                                                'dark:active:text-stone-300 dark:active:decoration-stone-500',
+                                                'dark:active:underline dark:active:underline-offset-8 dark:active:decoration-2',
+                                                'rounded-lg px-3 py-2 text-sm font-medium block w-1/3',
+                                            ],
+                                        )}
+                                        aria-current={route().current(item.route) ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </DisclosureButton>
+                                ))}
+                            </div>
+                        </DisclosurePanel>
                     </div>
                 </div>
             )}
--- resources/layouts/guest-layout.tsx
+++ resources/layouts/guest-layout.tsx
@@ -1,8 +1,7 @@
-import { PropsWithChildren } from 'react';
 import { Head } from '@inertiajs/react';
 
 import { Footer } from '@/components/guest/footer';
-import { Navbar } from '@/components/guest/navbar';
+import { GuestNavbar } from '@/components/guest/guest-navbar';
 
 export function GuestLayout({ children, title }: { children: React.ReactNode; title: string }) {
     return (
@@ -10,7 +9,7 @@ export function GuestLayout({ children, title }: { children: React.ReactNode; ti
             <Head title={title} />
 
             <div className="h-full min-h-screen min-w-sm flex flex-col bg-stone-100 dark:bg-stone-900">
-                <Navbar showLogo={!route().current('welcome')} />
+                <GuestNavbar showLogo={!route().current('welcome')} />
 
                 <main className="grow">{children}</main>
 
