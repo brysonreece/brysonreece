cf2386567e1637f755f80192af908d4034d0ab1a
Bryson Reece <hey@bryson.cc>
2025-03-12 23:46:10 -0500

    Add guest page transition

--- resources/layouts/guest-layout.tsx
+++ resources/layouts/guest-layout.tsx
@@ -1,9 +1,25 @@
-import { Head } from '@inertiajs/react';
+import { Head, router } from '@inertiajs/react';
+
+import { useEffect, useMemo, useState } from 'react';
+
+import { cn } from '@/lib/utils';
 
 import { GuestFooter } from '@/components/guest/guest-footer';
 import { GuestNavbar } from '@/components/guest/guest-navbar';
 
 export function GuestLayout({ children, title }: { children: React.ReactNode; title?: string }) {
+    const [transitioning, setTransitioning] = useState(false)
+
+    useEffect(() => {
+      router.on('start', () => setTransitioning(true))
+      router.on('finish', () => setTransitioning(false))
+    }, [])
+
+    const transitionClasses = useMemo(
+      () => `transition ${transitioning ? 'duration-0 opacity-0' : 'duration-300 opacity-100'}`,
+      [transitioning],
+    )
+
     return (
         <>
             {title && <Head title={title} />}
@@ -11,7 +27,7 @@ export function GuestLayout({ children, title }: { children: React.ReactNode; ti
             <div className="flex h-full min-h-screen min-w-sm flex-col bg-stone-100 dark:bg-stone-900">
                 <GuestNavbar showLogo={!route().current('guest.welcome')} />
 
-                <main className="grow">{children}</main>
+                <main className={cn('grow', transitionClasses)}>{children}</main>
 
                 <GuestFooter />
             </div>
