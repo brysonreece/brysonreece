02f0e40cc89031b3a05806d4b7c07255fdc31b83
Bryson Reece <hey@bryson.cc>
2025-03-12 22:14:23 -0500

    Simplify web route definitions

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,17 @@ use App\Http\Controllers\GuestController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [GuestController::class, 'welcome'])->name('guest.welcome');
-
-foreach (['about', 'career', 'projects', 'community', 'uses'] as $page) {
-    Route::get("/{$page}", [GuestController::class, $page])->name("guest.{$page}");
-}
+Route::controller(GuestController::class)->group(function () {
+    Route::get('/', 'welcome')->name('guest.welcome');
+    Route::get('about', 'about')->name('guest.about');
+    Route::get('career', 'career')->name('guest.career');
+    Route::get('projects', 'projects')->name('guest.projects');
+    Route::get('community', 'community')->name('guest.community');
+    Route::get('uses', 'uses')->name('guest.uses');
+});
 
 Route::middleware(['local', 'auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('dashboard', fn () => Inertia::render('dashboard'))->name('dashboard');
 });
 
 require __DIR__.'/settings.php';
