d5f4b0f8ad7c71fad951cfa96ecb6fa662503c9b
Bryson Reece <bryson.reece@flywheeldigital.com>
2025-08-04 00:14:39 -0500

    Add external link component

---
+++ resources/components/ui/external-link.tsx
@@ -0,0 +1,11 @@
+import { ComponentProps } from 'react';
+
+export function ExternalLink({ target, rel, ...props }: ComponentProps<'a'>) {
+    return (
+        <a
+            target={target || '_blank'}
+            rel={rel || 'noopener noreferrer'}
+            {...props}
+        />
+    );
+};
