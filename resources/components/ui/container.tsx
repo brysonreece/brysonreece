05843eaebd8ac23380eb3412d4470611300b8b90
Bryson Reece <hey@bryson.cc>
2025-08-20 10:22:33 -0500

    Update container padding below sm breakpoint

--- resources/components/ui/container.tsx
+++ resources/components/ui/container.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const Container = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-    <div ref={ref} className={cn('mx-auto max-w-7xl px-2 sm:px-6 lg:px-8', className)} {...props} />
+    <div ref={ref} className={cn('mx-auto max-w-7xl px-4 sm:px-6 lg:px-8', className)} {...props} />
 ));
 Container.displayName = 'Container';
 
