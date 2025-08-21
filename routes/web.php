0116285cd7fea040d02c47835430648dbdb90d52
Bryson Reece <hey@bryson.cc>
2025-08-21 00:50:38 -0500

    Add Emojicon subdomain redirect

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ Route::domain($host)
     ->group(__DIR__.'/web/app.php')
     ->group(__DIR__.'/web/settings.php');
 
+Route::domain("emojicon.{$host}")
+    ->get('/', fn () => redirect('https://emojicon.dev'))
+    ->name('subdomains.emojicon');
+
 Route::domain("links.{$host}")
     ->get('/', fn () => inertia('subdomains/links'))
     ->name('subdomains.links');
