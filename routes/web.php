2f2a1046d12abe67e8fd160dc1908cfbd46165c0
Bryson Reece <hey@bryson.cc>
2025-08-25 02:55:09 -0500

    Update Emojicon to Emojicons

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Uri;
 
@@ -12,9 +14,36 @@ Route::domain($host)
     ->group(__DIR__.'/web/app.php')
     ->group(__DIR__.'/web/settings.php');
 
-Route::domain("emojicon.{$host}")
-    ->get('/', fn () => redirect('https://emojicon.dev'))
-    ->name('subdomains.emojicon');
+Route::domain("emojicon.dev")->group(function () {
+    Route::get('/', fn () => inertia('subdomains/emojicons'))
+        ->name('emojicon.welcome');
+
+    Route::get('/{emoji}', fn (Request $request, string $emoji) => Response::make(
+        view('emojicons.svg', ['emoji' => $emoji]),
+        200,
+        ['Content-Type' => 'image/svg+xml']
+    ))->name('emojicon.svg');
+});
+
+Route::domain("emojicon.dev")
+    ->get('/', fn () => redirect('https://emojicons.dev'))
+    ->name('emojicon.dev');
+
+Route::domain("emojicons.{$host}")->group(function () {
+    Route::get('/', fn () => match (true) {
+        app()->isProduction() => redirect('https://emojicons.dev'),
+        default => inertia('subdomains/emojicons'),
+    })->name('subdomains.emojicons');
+
+    Route::get('/{emoji}', fn (Request $request, string $emoji) =>  match (true) {
+        app()->isProduction() => redirect("https://emojicons.dev/{$emoji}"),
+        default => Response::make(
+            view('emojicons.svg', ['emoji' => $emoji]),
+            200,
+            ['Content-Type' => 'image/svg+xml']
+        ),
+    })->name('subdomains.emojicons.svg');
+});
 
 Route::domain("links.{$host}")
     ->get('/', fn () => inertia('subdomains/links'))
