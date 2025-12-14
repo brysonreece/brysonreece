cec5175b5e0033180fd4024fdfb7e08112eae0e4
Bryson Reece <hey@bryson.cc>
2025-12-13 18:45:45 -0600

    Fix Inertia page mapping

--- config/inertia.php
+++ config/inertia.php
@@ -24,6 +24,35 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Pages
+    |--------------------------------------------------------------------------
+    |
+    | Set `ensure_pages_exist` to true if you want to enforce that Inertia page
+    | components exist on disk when rendering a page. This is useful for
+    | catching missing or misnamed components.
+    |
+    | The `page_paths` and `page_extensions` options define where to look
+    | for page components and which file extensions to consider.
+    |
+    */
+
+    'ensure_pages_exist' => false,
+
+    'page_paths' => [
+        resource_path('pages'),
+    ],
+
+    'page_extensions' => [
+        'js',
+        'jsx',
+        'svelte',
+        'ts',
+        'tsx',
+        'vue',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
     | Testing
     |--------------------------------------------------------------------------
     |
@@ -32,6 +61,9 @@ return [
     | attempts to locate the component as a file relative to the paths.
     |
     */
+    'page_paths' => [
+        resource_path('pages'),
+    ],
 
     'testing' => [
 
